eeefe609ec8910e7a46afe6adeb2691802a89bed
Stefano Morciano <stefano.morciano@banzai.it>
2016-06-17 17:50:28 +0200

    Esempio migliore di chiamata, ma probabilmente sposto questa roba dei commenti nel tema

--- js/libcomments.js
+++ js/libcomments.js
@@ -1,7 +1,8 @@
 wp.api.loadPromise.done( function() {
 	
 	var commentsCollection = new wp.api.collections.Comments();
-	var comments = commentsCollection.fetch();
-	console.log(comments);
+	var comments = commentsCollection.fetch({data: {"post": libComments.post_id}}).done(function(comments) {
+		console.log(comments);
+	});
 
 } )
